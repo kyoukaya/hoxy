6df6efdd304fd86bdb250131772ccda01a8ddfa3
kyoukaya <kyoukaya@gmail.com>
2019-09-30 00:51:16 +1000

    Rewrite parser

--- proxy/json/refscanner.go
+++ proxy/json/refscanner.go
@@ -12,16 +12,27 @@ type refScanner struct {
 type RefKind uint32
 
 const (
-	Int RefKind = iota
+	EmptyKind RefKind = 0
+	String    RefKind = 1 << iota
+	Int
 	Float
-	String
 	Array
 	Object
 	ObjectKey
 )
 
 func (n RefKind) String() string {
+	if n&String != 0 {
+		switch n {
+		case String | Int:
+			return "string_int"
+		case String | Float:
+			return "string_float"
+		}
+	}
 	switch n {
+	case EmptyKind:
+		return "empty"
 	case Int:
 		return "int"
 	case Float:
@@ -32,17 +43,17 @@ func (n RefKind) String() string {
 		return "array"
 	case Object:
 		return "object"
-	default:
-		panic(fmt.Sprintf("Undefined RefKind %#v", n))
-		return ""
 	}
+	panic(fmt.Sprintf("Undefined RefKind %#v", n))
 }
 
 type RefNode struct {
-	key      string
-	parent   *RefNode
-	children []*RefNode
-	kind     RefKind
+	key           string
+	parent        *RefNode
+	children      []*RefNode
+	kind          RefKind
+	precission    uint8
+	isEmptyString bool
 }
 
 func scanStateString(i int) string {
@@ -72,117 +83,152 @@ func scanStateString(i int) string {
 	}
 }
 
-func (cur *RefNode) assignKind(b []byte) {
-	if len(b) == 0 {
-		return
+func (cur *RefNode) assignKind(buf *bytes.Buffer) error {
+	if buf.Len() == 0 {
+		return fmt.Errorf("Something went wrong")
+	}
+	if sl := buf.Bytes(); sl[0] == '"' && sl[len(sl)-1] == '"' {
+		cur.kind = String
+		if len(sl) == 2 {
+			cur.isEmptyString = true
+			return nil
+		}
+		// Trim quotation marks for number processing
+		buf = bytes.NewBuffer(sl[1 : len(sl)-1])
 	}
 
-	var isNumber, isFp bool
-	s := string(b)
-	isNumber = true
-	isFp = false
-	for _, r := range s {
+	isNumber := true
+	isFp := false
+	var precision uint8 = 0
+	r, _, buffErr := buf.ReadRune()
+	for ; buffErr == nil; r, _, buffErr = buf.ReadRune() {
+		if isFp {
+			precision++
+		}
 		if r < '0' || r > '9' {
 			if r != '.' {
+				isNumber = false
+				break
+			}
+			if isFp {
 				isNumber = false
 				break
 			}
 			isFp = true
 		}
 	}
+
+	if !isNumber {
+		return nil
+	}
+	if isFp {
+		cur.kind |= Float
+		cur.precission = precision
+		return nil
+	}
+	cur.kind |= Int
+	return nil
 }
 
+// ParseRef parses input JSON data and returns a reference syntax tree.
 func ParseRef(data []byte) (*RefNode, error) {
 	scan := &refScanner{}
-	var root *RefNode
-	var cur *RefNode
+	root := &RefNode{}
+	cur := root
 	scan.reset()
-	accumulator := bytes.Buffer{}
+	buf := bytes.Buffer{}
 	for _, c := range data {
 		scan.bytes++
 		step := scan.step(&scan.scanner, c)
+		if step == scanError {
+			return root, scan.err
+		}
 		stepStr := scanStateString(step)
 		fmt.Println(stepStr)
 		switch step {
 		case scanBeginLiteral:
-			accumulator.Reset()
-			accumulator.WriteByte(c)
+			buf.Reset()
+			buf.WriteByte(c)
 		case scanContinue:
-			accumulator.WriteByte(c)
+			buf.WriteByte(c)
 		case scanObjectKey:
-			new := &RefNode{
-				parent: cur,
-				kind:   ObjectKey,
-			}
-			key := bytes.Trim(accumulator.Bytes(), `"`)
+			key := bytes.Trim(buf.Bytes(), `"`)
 			cur.key = string(key)
-			cur.children = append(cur.children, new)
-			cur = new
-			fmt.Println(cur.key)
-			accumulator.Reset()
+			cur.kind = ObjectKey
+			cur = &RefNode{
+				parent: cur,
+			}
+			fmt.Println(buf.String())
+			buf.Reset()
 		case scanArrayValue:
-			if accumulator.Len() > 0 {
-				new := &RefNode{
-					parent: cur,
-				}
-				new.assignKind(accumulator.Bytes())
-				cur.children = append(cur.children, new)
-				fmt.Println(accumulator.String())
-				accumulator.Reset()
+			if buf.Len() != 0 {
+				cur.assignKind(&buf)
 			}
+			cur.parent.children = append(cur.parent.children, cur)
+			cur = &RefNode{
+				parent: cur.parent,
+			}
+			fmt.Println(buf.String())
+			buf.Reset()
 		case scanEndArray:
-			if accumulator.Len() > 0 {
-				new := &RefNode{
-					parent: cur,
-				}
-				new.assignKind(accumulator.Bytes())
-				cur.children = append(cur.children, new)
-				cur = cur.parent
-				fmt.Println(accumulator.String())
-				accumulator.Reset()
+			// Could contain an object or array here
+			if cur.kind == EmptyKind {
+				cur.assignKind(&buf)
 			}
+			if buf.Len() > 0 || len(cur.children) > 0 {
+				cur.parent.children = append(cur.parent.children, cur)
+			}
+			cur = cur.parent
+			fmt.Println(buf.String())
+			buf.Reset()
 		case scanEndObject:
-			if accumulator.Len() > 0 {
-				cur.assignKind(accumulator.Bytes())
-				fmt.Println(accumulator.String())
-				accumulator.Reset()
-				cur = cur.parent
+			if cur.kind == EmptyKind {
+				cur.assignKind(&buf)
+				fmt.Println(buf.String())
+				buf.Reset()
 			}
-			cur = cur.parent
+			// We can be in the scanEndObject state if the root value is an empty object
+			// In that case, cur.parent would be nil as there would be no ObjectKey.
+			if cur.parent != nil {
+				break
+			}
+			rootObj := cur.parent.parent
+			rootObj.parent.children = append(rootObj.parent.children, rootObj)
+			cur = &RefNode{
+				parent: rootObj.parent,
+			}
 		case scanObjectValue:
-			cur.assignKind(accumulator.Bytes())
-			fmt.Println(accumulator.String())
-			accumulator.Reset()
-			cur = cur.parent
+			if cur.kind == EmptyKind {
+				cur.assignKind(&buf)
+			}
+			cur.parent.children = append(cur.parent.children, cur)
+			cur = &RefNode{
+				// This node's immediate parent is the ObjectKey node, so we take its
+				// parent again to get to the actual object node.
+				parent: cur.parent.parent,
+			}
+			fmt.Println(buf.String())
+			buf.Reset()
 		case scanBeginArray:
+			cur.kind = Array
 			new := &RefNode{
 				parent: cur,
-				kind:   Array,
-			}
-			if cur == nil {
-				root = new
-			} else {
-				cur.children = append(cur.children, new)
 			}
 			cur = new
 		case scanBeginObject:
+			cur.kind = Object
 			new := &RefNode{
 				parent: cur,
-				kind:   Object,
-			}
-			if cur == nil {
-				root = new
-			} else {
-				cur.children = append(cur.children, new)
 			}
 			cur = new
-		}
-		if step == scanError {
-			return root, scan.err
 		}
 	}
 	if scan.eof() == scanError {
 		return root, scan.err
+	}
+	if buf.Len() != 0 {
+		// assignKind when completed
+		cur.kind = Int
 	}
 	return root, nil
 }
--- proxy/json/scanner_test.go
+++ proxy/json/scanner_test.go
@@ -1,52 +1,94 @@
-package json_test
+package json
 
 import (
+	"bytes"
 	"testing"
-
-	"github.com/kyoukaya/hoxy/proxy/json"
 )
+
+func TestAssignKind(t *testing.T) {
+	node := &RefNode{}
+	node.assignKind(bytes.NewBuffer([]byte(`""`)))
+	if node.kind != String || !node.isEmptyString {
+		t.Errorf("Expected empty string, got %#v", node)
+	}
+	node = &RefNode{}
+	node.assignKind(bytes.NewBuffer([]byte(`"1"`)))
+	if node.kind != Int|String {
+		t.Errorf("Expected Int|String, got %#v", node.kind.String())
+	}
+	node = &RefNode{}
+	node.assignKind(bytes.NewBuffer([]byte(`1`)))
+	if node.kind != Int {
+		t.Errorf("Expected Int, got %#v", node.kind.String())
+	}
+	node = &RefNode{}
+	node.assignKind(bytes.NewBuffer([]byte(`"1.1"`)))
+	if node.kind != Float|String && node.precission == 1 {
+		t.Errorf("Expected Float|String, got %#v", node.kind.String())
+	}
+	node = &RefNode{}
+	node.assignKind(bytes.NewBuffer([]byte(`"1.11"`)))
+	if node.kind != Float|String && node.precission == 2 {
+		t.Errorf("Expected Float|String, got %#v", node.kind.String())
+	}
+	node = &RefNode{}
+	node.assignKind(bytes.NewBuffer([]byte(`"1."`)))
+	if node.kind != Float|String && node.precission == 0 {
+		t.Errorf("Expected Float|String, got %#v", node.kind.String())
+	}
+	node = &RefNode{}
+	node.assignKind(bytes.NewBuffer([]byte(`"1..1"`)))
+	if node.kind != String {
+		t.Errorf("Expected String, got %#v", node.kind.String())
+	}
+	node = &RefNode{}
+	node.assignKind(bytes.NewBuffer([]byte(`1.11`)))
+	if node.kind != Float && node.precission == 2 {
+		t.Errorf("Expected Float|String, got %#v", node.kind.String())
+	}
+}
 
 func TestLiteral(t *testing.T) {
 	data := []byte(`1`)
-	json.ParseRef(data)
+	ParseRef(data)
 }
 
 func TestEmptySlice(t *testing.T) {
 	data := []byte(`[]`)
-	json.ParseRef(data)
+	ParseRef(data)
 }
 
 func TestEmptyObj(t *testing.T) {
 	data := []byte(`{}`)
-	json.ParseRef(data)
+	ParseRef(data)
 }
 
 func TestArrayWithLiterals(t *testing.T) {
 	data := []byte(`[1,2,3]`)
-	json.ParseRef(data)
+	ParseRef(data)
 }
 
 func TestArrayWithObj(t *testing.T) {
-	data := []byte(`[1,{"2":[3,4],"5":6}, [7]]`)
-	json.ParseRef(data)
+	data := []byte(`[1,{"2":[3,4],"5":6,"7":8}, [9]]`)
+	ParseRef(data)
 }
 
 func TestSliceWithSlice(t *testing.T) {
 	data := []byte(`[11,[2,3],4]`)
-	json.ParseRef(data)
+	ParseRef(data)
 }
 
 func TestObjWithObj(t *testing.T) {
 	data := []byte(`{"1":{}}`)
-	json.ParseRef(data)
+	ParseRef(data)
 }
 
 func TestObjEmptyKey(t *testing.T) {
 	data := []byte(`{"":1}`)
-	json.ParseRef(data)
+	ParseRef(data)
 }
 
 func TestObjWithSlice(t *testing.T) {
 	data := []byte(`{"1":[2]}`)
-	json.ParseRef(data)
+	ParseRef(data)
 }
